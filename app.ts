3bd8cf080a543821b394a607adcec3adcab524fd
Ud293nBqda@outlook.kr <johndoe@example.com>
2024-09-04 00:45:15 +0900

    Feat: FlowHandler 추가

--- app.ts
+++ app.ts
@@ -1,5 +1,8 @@
-import Hero, { ISuperNode } from "@ulixee/hero-playground";
+import Hero, { ISuperNode, Tab } from "@ulixee/hero-playground";
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
 import fs from "fs";
+
 
 interface IGameStats {
   homeTeam: ITeamStats;
@@ -16,11 +19,113 @@ interface IPlayerStats {
   [key: string]: string;
 }
 
+const extractGameInfoFromTab = async (tab: Tab, url: string) => {
+  try {
+    await tab.goto(url);
+    let title = await tab.document.title;
+    if (title.includes("Error")) {
+      await tab.goto(url);
+      title = await tab.document.title;
+    }
+
+    if (title.includes("Error")) {
+      throw new Error("Error loading the page");
+    }
+
+    // replace all spaces with underscores
+    title = title.replace(/\s/g, "_");
+
+    await tab.registerFlowHandler("Consent", assert => {
+      assert(tab.querySelector('body > div.fc-consent-root > div.fc-dialog-container > div.fc-dialog.fc-choice-dialog > div.fc-footer-buttons-container > div.fc-footer-buttons > button.fc-button.fc-cta-consent.fc-primary-button').$exists)
+    },
+    async error => {
+      console.error("Consent button found");
+      await tab.querySelector('body > div.fc-consent-root > div.fc-dialog-container > div.fc-dialog.fc-choice-dialog > div.fc-footer-buttons-container > div.fc-footer-buttons > button.fc-button.fc-cta-consent.fc-primary-button').$click();
+    });
+    /*
+    const consentButton = tab.querySelector('body > div.fc-consent-root > div.fc-dialog-container > div.fc-dialog.fc-choice-dialog > div.fc-footer-buttons-container > div.fc-footer-buttons > button.fc-button.fc-cta-consent.fc-primary-button');
+    try {
+      await tab.waitForElement(consentButton, {
+        timeoutMs: 5000,
+      });
+      await consentButton.click();
+    } catch (error) {
+      console.error("Consent button not found");
+    }
+    */
+
+    // extract scores
+    let scores : Array<Array<string | null>> = [];
+    try {
+      const summary = tab.querySelector("table[class^='GameLinescore_table']");
+      await tab.waitForElement(summary, {
+        timeoutMs: 5000,
+      });
+      scores = await extractTeamScores(summary);
+    } catch (error) {
+      console.error("Scores not found");
+      throw error;
+    }
+
+    // Click on the box score tab
+    try {
+      const boxScore = tab.querySelector("#box-score");
+      await tab.waitForElement(boxScore, {
+        timeoutMs: 5000,
+      });
+      await boxScore.click();
+    } catch (error) {
+      console.error("Box score tab not found");
+      throw error;
+    }
+
+    const sectionElements = tab.querySelectorAll('section[class^="GameBoxscore_gbTableSection"]');
+    await tab.waitForElement(sectionElements[0], {
+      timeoutMs: 5000,
+    });
+    let gameStats : IGameStats = {
+      homeTeam: {
+        teamName: "",
+        scores: [],
+        players: [],
+      },
+      awayTeam: {
+        teamName: "",
+        scores: [],
+        players: [],
+      },
+    };
+
+    await sectionElements.forEach(async (sectionElement, index) => {
+      const teamStats = await extractTeamStats(sectionElement);
+
+      if (index === 0) {
+        gameStats.awayTeam = teamStats;
+        gameStats.awayTeam.scores = scores[0];
+      } else {
+        gameStats.homeTeam = teamStats;
+        gameStats.homeTeam.scores = scores[1];
+      }
+    });
+
+    fs.writeFileSync(
+      `${title}.json`, 
+      JSON.stringify(gameStats, null, 2)
+    );
+  } catch (error) {
+    // save the page to a file for debugging using fs
+    const pageContent = await tab.document.documentElement.outerHTML;
+    fs.writeFileSync("error-page.html", pageContent);
+    console.error("An error occurred while scraping the page:", error);
+  }
+};
+
+
 const extractTeamScores = async (teamStatsElement: ISuperNode) => {
   const teamScores : Array<Array<string | null>> = [];
 
   const scores = teamStatsElement.querySelectorAll("tbody > tr");
-  await scores.forEach(async (scoreElement, index) => {
+  await scores.forEach(async (scoreElement) => {
     const teamScoresPerQt = scoreElement.querySelectorAll("td");
     const score : Array<string | null> = [];
     await teamScoresPerQt.forEach(async (scoreElement) => {
@@ -78,7 +183,6 @@ const extractTeamName = async (teamElement: ISuperNode) => {
   }
 }
 
-
 const extractPlayerName = async (playerElement: ISuperNode) => {
   try {
     const playerNameElement = playerElement.querySelector("span[class^='GameBoxscoreTablePlayer_gbpNameFull']");
@@ -107,11 +211,6 @@ const extractPlayerStats = async (playerElement: ISuperNode) => {
   if (!(await playerStats.length)) {
     return { ...stats };
   }
-  await playerStats[0].$waitForExists(
-    {
-      timeoutMs: 5000,
-    }
-  );
   await playerStats.forEach(async (statElement, index) => {
     stats[statsHeader[index]] = await statElement.textContent || "";
   });
@@ -121,74 +220,26 @@ const extractPlayerStats = async (playerElement: ISuperNode) => {
 
 (async () => {
   const hero = new Hero();
+  const readlineInterface = readline.createInterface({
+    input,
+    output,
+  });
 
-  try {
-    await hero.goto("https://www.nba.com/game/atl-vs-chi-1522400055");
-    const title = await hero.document.title;
-    console.log("Title:", title);
-
-    const consentButton = hero.querySelector('body > div.fc-consent-root > div.fc-dialog-container > div.fc-dialog.fc-choice-dialog > div.fc-footer-buttons-container > div.fc-footer-buttons > button.fc-button.fc-cta-consent.fc-primary-button');
-    try {
-      await hero.waitForElement(consentButton, {
-        timeoutMs: 10000,
-      });
-      if (consentButton) {
-        await consentButton.click();
-      }
-    } catch (error) {
-      console.error("Consent button not found");
+  while (1) {
+    const tabs = await hero.tabs;
+    console.log(`Current number of tabs: ${tabs.length}`);
+    const url = await readlineInterface.question("Enter the URL of the game: ");
+    if (url === "exit") {
+      break;
     }
 
-    // extract scores
-    const summary = hero.querySelector("table[class^='GameLinescore_table']");
-    await hero.waitForElement(summary, {
-      timeoutMs: 5000,
-    });
-    const scores = await extractTeamScores(summary);
-    console.log("Scores:", scores);
-
-    // Click on the box score tab
-    const boxScore = hero.querySelector("#box-score");
-    await hero.waitForElement(boxScore, {
-      timeoutMs: 30000,
-    });
-    await boxScore.click();
-
-    const sectionElements = hero.querySelectorAll('section[class^="GameBoxscore_gbTableSection"]');
-    await hero.waitForElement(sectionElements[0], {
-      timeoutMs: 30000,
-    });
-    let gameStats : IGameStats = {
-      homeTeam: {
-        teamName: "",
-        scores: [],
-        players: [],
-      },
-      awayTeam: {
-        teamName: "",
-        scores: [],
-        players: [],
-      },
-    };
-
-    await sectionElements.forEach(async (sectionElement, index) => {
-      const teamStats = await extractTeamStats(sectionElement);
-      if (index === 0) {
-        gameStats.awayTeam = teamStats;
-        gameStats.awayTeam.scores = scores[0];
-      } else {
-        gameStats.homeTeam = teamStats;
-        gameStats.homeTeam.scores = scores[1];
-      }
-    });
-
-    console.log("Game stats:", gameStats);
-    fs.writeFileSync("game-stats.json", JSON.stringify(gameStats, null, 2));
-  } catch (error) {
-    // save the page to a file for debugging using fs
-    const pageContent = await hero.document.documentElement.outerHTML;
-    fs.writeFileSync("error-page.html", pageContent);
-    console.error("An error occurred while scraping the page:", error);
+    const newTab = await hero.newTab();
+    console.log("New tab opened successfully");
+    await extractGameInfoFromTab(newTab, url);
+    console.log("Game stats extracted successfully");
+    await newTab.close();
   }
+
+  console.log("Exiting...");
   await hero.close();
 })();
